a08530dc7045e4df3dd714fcfd063904758b7f3a
C-Seven-FL <124684787+C-Seven-FL@users.noreply.github.com>
2025-05-12 04:47:12 +0200

    Add files via upload

---
+++ server(v2_no_abl_old_dao)/app.js
@@ -0,0 +1,34 @@
+//načtení modulu express
+const express = require("express");
+const cors = require("cors");
+
+const exerciseRouter = require("./controller/exercise-controller");
+const wrecordRouter = require("./controller/wrecord-controller");
+
+//inicializace nového Express.js serveru
+const app = express();
+//definování portu, na kterém má aplikace běžet na localhostu
+const port = process.env.PORT || 3000;
+
+// Parsování body
+app.use(express.json()); // podpora pro application/json
+app.use(express.urlencoded({ extended: true })); // podpora pro application/x-www-form-urlencoded
+
+app.use(cors())
+
+//jednoduchá definice routy s HTTP metodou GET, která pouze navrací text
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/exercise", exerciseRouter);
+app.use("/wrecord", wrecordRouter);
+
+app.get("/*", (req, res) => {
+  res.send("Unknown path!");
+});
+
+//nastavení portu, na kterém má běžet HTTP server
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
